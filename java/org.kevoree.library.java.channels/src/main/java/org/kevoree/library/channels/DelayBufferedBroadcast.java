8f3c79cf7372b1daed9c3b2ac83045a789402fb0
Grégory NAIN <gregory.nain@gmail.com>
2013-12-02 16:07:40 +0100

    Adding two other channel implems

---
+++ java/org.kevoree.library.java.channels/src/main/java/org/kevoree/library/channels/DelayBufferedBroadcast.java
@@ -0,0 +1,75 @@
+package org.kevoree.library.channels;
+
+import org.kevoree.annotation.*;
+import org.kevoree.api.Callback;
+import org.kevoree.api.ChannelContext;
+import org.kevoree.api.ChannelDispatch;
+import org.kevoree.api.Port;
+
+import java.util.LinkedList;
+import java.util.concurrent.*;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: duke
+ * Date: 28/11/2013
+ * Time: 10:05
+ */
+@ChannelType
+public class DelayBufferedBroadcast implements ChannelDispatch, Runnable {
+
+    class QueuedElement {
+        Object payload;
+        Callback callback;
+    }
+
+    @Param(optional = true)
+    long delay = 5000;
+
+    @KevoreeInject
+    ChannelContext channelContext;
+
+    private ScheduledExecutorService executor;
+    private ArrayBlockingQueue<QueuedElement> queue = new ArrayBlockingQueue<QueuedElement>(30);
+
+    public DelayBufferedBroadcast() {
+    }
+
+    @Start
+    public void channelStart() {
+        executor = Executors.newSingleThreadScheduledExecutor();
+        executor.scheduleAtFixedRate(this, delay, delay, TimeUnit.MILLISECONDS);
+    }
+
+    @Stop
+    public void channelStop() {
+        queue.clear();
+        executor.shutdownNow();
+        executor = null;
+    }
+
+
+    @Override
+    public void dispatch(Object payload, Callback callback) {
+        try {
+            QueuedElement e = new QueuedElement();
+            e.callback = callback;
+            e.payload = payload;
+            queue.put(e);
+        } catch (InterruptedException e1) {
+            e1.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+
+    }
+
+
+    public void run() {
+        while(!queue.isEmpty()) {
+            QueuedElement e = queue.poll();
+            for (Port p : channelContext.getLocalPorts()) {
+                p.call(e.payload, e.callback);
+            }
+        }
+    }
+
+}
---
+++ java/org.kevoree.library.java.channels/src/main/java/org/kevoree/library/channels/SizeBufferedBroadcast.java
@@ -0,0 +1,75 @@
+package org.kevoree.library.channels;
+
+import org.kevoree.annotation.*;
+import org.kevoree.api.Callback;
+import org.kevoree.api.ChannelContext;
+import org.kevoree.api.ChannelDispatch;
+import org.kevoree.api.Port;
+import org.kevoree.log.Log;
+
+import java.util.concurrent.*;
+
+/**
+ * Waits until the buffer is full to send the messages.
+ * The size of the buffer can be customized with the bufferSize property.
+ */
+@ChannelType
+public class SizeBufferedBroadcast implements ChannelDispatch, Runnable {
+
+    class QueuedElement {
+        Object payload;
+        Callback callback;
+    }
+
+    @Param(optional = true)
+    int bufferSize = 5;
+
+    @KevoreeInject
+    ChannelContext channelContext;
+
+    private ExecutorService executor;
+    private ArrayBlockingQueue<QueuedElement> queue = new ArrayBlockingQueue<QueuedElement>(bufferSize + 3);
+
+    public SizeBufferedBroadcast() {
+    }
+
+    @Start
+    public void channelStart() {
+        executor = Executors.newSingleThreadExecutor();
+    }
+
+    @Stop
+    public void channelStop() {
+        queue.clear();
+        executor.shutdownNow();
+        executor = null;
+    }
+
+
+    @Override
+    public void dispatch(Object payload, Callback callback) {
+        try {
+            QueuedElement e = new QueuedElement();
+            e.callback = callback;
+            e.payload = payload;
+            queue.put(e);
+        } catch (InterruptedException e1) {
+            e1.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+        if(queue.size() >= bufferSize) {
+            executor.submit(this);
+        }
+
+    }
+
+
+    public void run() {
+        while(!queue.isEmpty()) {
+            QueuedElement e = queue.poll();
+            for (Port p : channelContext.getLocalPorts()) {
+                p.call(e.payload, e.callback);
+            }
+        }
+    }
+
+}
