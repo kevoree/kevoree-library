6cf7ac33b28623de2ea9d589def9e1bab91b4354
Francois Fouquet <fouquet.f@gmail.com>
2013-12-05 16:05:06 +0100

    add load balancer for first step of tutorial

---
+++ java/org.kevoree.library.java.channels/src/main/java/org/kevoree/library/channels/LoadBalancer.java
@@ -0,0 +1,52 @@
+package org.kevoree.library.channels;
+
+import org.kevoree.annotation.ChannelType;
+import org.kevoree.annotation.KevoreeInject;
+import org.kevoree.annotation.Start;
+import org.kevoree.annotation.Stop;
+import org.kevoree.api.Callback;
+import org.kevoree.api.ChannelContext;
+import org.kevoree.api.ChannelDispatch;
+import org.kevoree.api.Port;
+
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * Created by duke on 05/12/2013.
+ */
+
+@ChannelType
+public class LoadBalancer implements ChannelDispatch {
+
+    @KevoreeInject
+    ChannelContext channelContext;
+
+    ExecutorService executor = null;
+
+    @Start
+    public void start() {
+        executor = Executors.newSingleThreadExecutor();
+    }
+
+    @Stop
+    public void stop() {
+        executor.shutdownNow();
+    }
+
+    private Random random = new Random();
+
+    @Override
+    public void dispatch(final Object payload, final Callback callback) {
+        executor.submit(new Runnable() {
+            @Override
+            public void run() {
+                List<Port> ports = channelContext.getLocalPorts();
+                Port selected = ports.get(random.nextInt(ports.size()));
+                selected.call(payload, callback);
+            }
+        });
+    }
+}
