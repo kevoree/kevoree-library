02977b6e4bcae8d31d86851bf0ef96b2eddba8fc
Francois Fouquet <fouquet.f@gmail.com>
2014-01-09 00:58:01 +0100

    fix according to new factory

--- cloud/org.kevoree.library.cloud.lightlxc/src/main/java/org/kevoree/library/cloud/lightlxc/wrapper/LightLXCWrapperFactory.kt
+++ cloud/org.kevoree.library.cloud.lightlxc/src/main/java/org/kevoree/library/cloud/lightlxc/wrapper/LightLXCWrapperFactory.kt
@@ -5,6 +5,7 @@ import org.kevoree.modeling.api.KMFContainer
 import org.kevoree.api.BootstrapService
 import org.kevoree.library.defaultNodeTypes.wrapper.KInstanceWrapper
 import org.kevoree.ContainerNode
+import org.kevoree.api.ModelService
 
 /**
  * Created with IntelliJ IDEA.
@@ -15,13 +16,13 @@ import org.kevoree.ContainerNode
 
 class LightLXCWrapperFactory(nodeName: String) : WrapperFactory(nodeName) {
 
-    override fun wrap(modelElement: KMFContainer, newBeanInstance: Any, tg: ThreadGroup, bs: BootstrapService): KInstanceWrapper {
+    override fun wrap(modelElement: KMFContainer, newBeanInstance: Any, tg: ThreadGroup, bs: BootstrapService,modelService: ModelService): KInstanceWrapper {
         when(modelElement) {
             is ContainerNode -> {
                 return LightLXCNodeWrapper(modelElement, newBeanInstance, tg, bs)
             }
             else -> {
-                return super.wrap(modelElement, newBeanInstance, tg, bs)
+                return super.wrap(modelElement, newBeanInstance, tg, bs,modelService)
             }
         }
     }
