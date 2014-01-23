9c127be736fc82a7cac1725feeeb5f92a1cf34c2
Maxime Tricoire <max.tricoire@gmail.com>
2014-01-23 11:45:38 +0100

    merge npm work in progress

--- java/org.kevoree.library.java.editor/src/main/java/org/kevoree/library/java/editor/handler/MergeHandler.java
+++ java/org.kevoree.library.java.editor/src/main/java/org/kevoree/library/java/editor/handler/MergeHandler.java
@@ -1,6 +1,5 @@
 package org.kevoree.library.java.editor.handler;
 
-import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.google.gson.JsonPrimitive;
--- java/org.kevoree.library.java.editor/src/main/java/org/kevoree/library/java/editor/service/merge/NpmMergeService.java
+++ java/org.kevoree.library.java.editor/src/main/java/org/kevoree/library/java/editor/service/merge/NpmMergeService.java
@@ -1,10 +1,17 @@
 package org.kevoree.library.java.editor.service.merge;
 
 import org.kevoree.ContainerRoot;
+import org.kevoree.compare.DefaultModelCompare;
 import org.kevoree.impl.DefaultKevoreeFactory;
 import org.kevoree.library.java.editor.model.Library;
+import org.kevoree.loader.JSONModelLoader;
+import org.kevoree.log.Log;
+import org.kevoree.npm.resolver.NpmResolver;
 
+import java.io.File;
 import java.util.Collection;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 
 /**
  * Created by leiko on 23/01/14.
@@ -15,6 +22,30 @@ public class NpmMergeService implements MergeService {
     public ContainerRoot process(Collection<Library> libraries) {
         DefaultKevoreeFactory factory = new DefaultKevoreeFactory();
         ContainerRoot model = factory.createContainerRoot();
+        DefaultModelCompare compare = new DefaultModelCompare();
+        JSONModelLoader loader = new JSONModelLoader();
+        NpmResolver resolver = new NpmResolver();
+
+        for (Library lib : libraries) {
+            for (String version : lib.getVersions()) {
+                try {
+                    File resolved = resolver.resolve(lib.getArtifactId(), version);
+                    if (resolved != null && resolved.exists()) {
+                        ZipFile tarball = new ZipFile(new File(resolved.getAbsolutePath()));
+                        ZipEntry entry = tarball.getEntry("kevlib.json");
+                        if (entry != null) {
+                            ContainerRoot remoteModel = (ContainerRoot) loader.loadModelFromStream(tarball.getInputStream(entry)).get(0);
+                            compare.merge(model, remoteModel).applyOn(model);
+                        }
+                    } else {
+                        Log.warn("Can't resolve TypeDefinition {}:{}", lib.getArtifactId(), version);
+                    }
+                } catch (Exception e) {
+                    Log.error(e.getMessage());
+                }
+            }
+        }
+
         return model;
     }
 }
