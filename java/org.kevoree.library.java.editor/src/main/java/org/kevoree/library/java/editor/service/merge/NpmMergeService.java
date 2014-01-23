199597fcf822e594e0f0ff5b2e7c8e8b2d376bf1
Maxime Tricoire <max.tricoire@gmail.com>
2014-01-23 14:26:05 +0100

    WebEditor /merge now works

--- java/org.kevoree.library.java.editor/src/main/java/org/kevoree/library/java/editor/service/merge/NpmMergeService.java
+++ java/org.kevoree.library.java.editor/src/main/java/org/kevoree/library/java/editor/service/merge/NpmMergeService.java
@@ -1,5 +1,7 @@
 package org.kevoree.library.java.editor.service.merge;
 
+import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
+import org.apache.commons.compress.archivers.tar.TarArchiveInputStream;
 import org.kevoree.ContainerRoot;
 import org.kevoree.compare.DefaultModelCompare;
 import org.kevoree.impl.DefaultKevoreeFactory;
@@ -8,10 +10,10 @@ import org.kevoree.loader.JSONModelLoader;
 import org.kevoree.log.Log;
 import org.kevoree.npm.resolver.NpmResolver;
 
+import java.io.ByteArrayInputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.util.Collection;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipFile;
 
 /**
  * Created by leiko on 23/01/14.
@@ -31,17 +33,28 @@ public class NpmMergeService implements MergeService {
                 try {
                     File resolved = resolver.resolve(lib.getArtifactId(), version);
                     if (resolved != null && resolved.exists()) {
-                        ZipFile tarball = new ZipFile(new File(resolved.getAbsolutePath()));
-                        ZipEntry entry = tarball.getEntry("kevlib.json");
-                        if (entry != null) {
-                            ContainerRoot remoteModel = (ContainerRoot) loader.loadModelFromStream(tarball.getInputStream(entry)).get(0);
-                            compare.merge(model, remoteModel).applyOn(model);
+                        TarArchiveInputStream tarInput = new TarArchiveInputStream(new FileInputStream(resolved));
+                        try {
+                            TarArchiveEntry entry;
+                            while (null!=(entry=tarInput.getNextTarEntry())) {
+                                if (entry.getName().equals("package/kevlib.json")) {
+                                    int entrySize = (int) entry.getSize();
+                                    byte[] content = new byte[entrySize];
+                                    tarInput.read(content, 0, entrySize);
+                                    ContainerRoot pkgModel = (ContainerRoot) loader.loadModelFromStream(new ByteArrayInputStream(content)).get(0);
+                                    compare.merge(model, pkgModel).applyOn(model);
+                                }
+                            }
+                        } catch (Exception e) {
+                            Log.error(e.getMessage());
+                        } finally {
+                            tarInput.close();
                         }
                     } else {
-                        Log.warn("Can't resolve TypeDefinition {}:{}", lib.getArtifactId(), version);
+                        Log.warn("NpmMergeService: Can't resolve TypeDefinition {}:{}", lib.getArtifactId(), version);
                     }
                 } catch (Exception e) {
-                    Log.error(e.getMessage());
+                    Log.error("NpmMergeService: "+e.getMessage());
                 }
             }
         }
