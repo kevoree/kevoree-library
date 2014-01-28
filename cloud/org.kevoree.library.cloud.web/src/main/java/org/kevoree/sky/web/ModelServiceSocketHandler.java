a5825a7f93b026ab46463f902d63811aebf50f4d
Francois Fouquet <fouquet.f@gmail.com>
2014-01-28 01:45:22 +0100

    cleaned version

--- cloud/org.kevoree.library.cloud.web/src/main/java/org/kevoree/sky/web/ModelServiceSocketHandler.java
+++ cloud/org.kevoree.library.cloud.web/src/main/java/org/kevoree/sky/web/ModelServiceSocketHandler.java
@@ -1,12 +1,10 @@
 package org.kevoree.sky.web;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-import org.json.JSONStringer;
+
 import org.kevoree.ContainerRoot;
 import org.kevoree.api.ModelService;
 import org.kevoree.api.handler.ModelListener;
-import org.kevoree.loader.JSONModelLoader;
+import org.kevoree.api.handler.UpdateCallback;
 import org.kevoree.log.Log;
 import org.kevoree.modeling.api.trace.TraceSequence;
 import org.kevoree.serializer.JSONModelSerializer;
@@ -17,9 +15,6 @@ import org.webbitserver.WebSocketConnection;
 import java.io.ByteArrayOutputStream;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * Created with IntelliJ IDEA.
@@ -32,39 +27,22 @@ public class ModelServiceSocketHandler extends BaseWebSocketHandler implements M
     private ModelService modelService = null;
     private ArrayList<WebSocketConnection> connections = new ArrayList<WebSocketConnection>();
     private JSONModelSerializer jsonSaver = new JSONModelSerializer();
-    private JSONModelLoader jsonLoader = new JSONModelLoader();
 
-    private Map<WebSocketConnection, TraceSequence> updatesToApply;
-    private Map<WebSocketConnection, Integer> updatesRetried;
-
-    private UpdateApplier updateApplier;
-
-    public ModelServiceSocketHandler(ModelService _modelService, long delayWhenNothing, long maxRetry) {
+    public ModelServiceSocketHandler(ModelService _modelService) {
         modelService = _modelService;
-        updatesToApply = Collections.synchronizedMap(new HashMap<WebSocketConnection, TraceSequence>());
-        updatesRetried = Collections.synchronizedMap(new HashMap<WebSocketConnection, Integer>());
-        updateApplier = new UpdateApplier(true, delayWhenNothing, maxRetry, updatesToApply, updatesRetried, modelService, this);
-        new Thread(updateApplier).start();
         modelService.registerModelListener(this);
     }
 
     public void destroy() {
         modelService.unregisterModelListener(this);
-        updateApplier.shutdown();
-        updatesToApply.clear();
-        updatesToApply = null;
-        updatesRetried.clear();
-        updatesRetried = null;
     }
 
     private void sendModel(WebSocketConnection connection) {
         try {
             ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
             jsonSaver.serializeToStream(modelService.getCurrentModel().getModel(), outputStream);
-            connection.send(new JSONStringer().object().key("model").value(outputStream.toString("UTF-8")).endObject().toString());
+            connection.send("model=" + outputStream.toString("UTF-8"));
         } catch (UnsupportedEncodingException e) {
-            Log.error("Unable to send the current model", e);
-        } catch (JSONException e) {
             Log.error("Unable to send the current model", e);
         }
     }
@@ -78,20 +56,26 @@ public class ModelServiceSocketHandler extends BaseWebSocketHandler implements M
         connections.remove(connection);
     }
 
-    public void onMessage(WebSocketConnection connection, String message) {
+    public void onMessage(final WebSocketConnection connection, String message) {
         Log.trace("Receiving request: {}", message);
         try {
-            JSONObject jsonReader = new JSONObject(message);
-            if (jsonReader.get("diff") != null) {
-                connection.send(new JSONStringer().object().key("event").value("update").endObject().toString());
-                TraceSequence sequence = new DefaultTraceSequence().populateFromString(jsonReader.get("diff").toString());
-                TraceSequence previous = updatesToApply.get(connection);
-                if (previous != null) {
-                    sequence.append(previous);
-                }
-                updatesToApply.put(connection, sequence);
-            }
-        } catch (JSONException e) {
+            //JSONObject jsonReader = new JSONObject(message);
+            //if (jsonReader.get("diff") != null) {
+                connection.send("{\"event\":\"update\"}");
+                final TraceSequence sequence = new DefaultTraceSequence().populateFromString(message);
+                modelService.submitSequence(sequence, new UpdateCallback() {
+                    @Override
+                    public void run(Boolean applied) {
+                        if (applied) {
+                            connection.send("{\"event\":\"done\"}");
+                        } else {
+                            connection.send("{\"event\":\"error\"}");
+                        }
+                        Log.info("model updated : {}", applied);
+                    }
+                });
+            //}
+        } catch (Exception e) {
             Log.warn("unable to manage the type of message: {}", e, message);
         }
     }
@@ -100,12 +84,11 @@ public class ModelServiceSocketHandler extends BaseWebSocketHandler implements M
         try {
             ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
             jsonSaver.serializeToStream(modelService.getCurrentModel().getModel(), outputStream);
+            String payload = "model=" + outputStream.toString("UTF-8");
             for (WebSocketConnection con : connections) {
-                con.send(new JSONStringer().object().key("model").value(outputStream.toString("UTF-8")).endObject().toString());
+                con.send(payload);
             }
         } catch (UnsupportedEncodingException e) {
-            Log.error("Unable to send the current model", e);
-        } catch (JSONException e) {
             Log.error("Unable to send the current model", e);
         }
     }
@@ -127,7 +110,7 @@ public class ModelServiceSocketHandler extends BaseWebSocketHandler implements M
 
     @Override
     public void modelUpdated() {
-            broadcastMessage();
+        broadcastMessage();
     }
 
     @Override
@@ -137,4 +120,5 @@ public class ModelServiceSocketHandler extends BaseWebSocketHandler implements M
     @Override
     public void postRollback(ContainerRoot containerRoot, ContainerRoot containerRoot2) {
     }
+
 }
--- cloud/org.kevoree.library.cloud.web/src/main/java/org/kevoree/sky/web/WebFrontend.java
+++ cloud/org.kevoree.library.cloud.web/src/main/java/org/kevoree/sky/web/WebFrontend.java
@@ -3,8 +3,9 @@ package org.kevoree.sky.web;
 import org.kevoree.annotation.*;
 import org.kevoree.api.ModelService;
 import org.kevoree.log.Log;
-import org.webbitserver.WebServer;
-import org.webbitserver.WebServers;
+import org.webbitserver.*;
+
+import java.nio.charset.Charset;
 
 /**
  * Created with IntelliJ IDEA.
@@ -13,7 +14,7 @@ import org.webbitserver.WebServers;
  * Time: 11:52
  */
 @ComponentType
-public class WebFrontend {
+public class WebFrontend implements HttpHandler {
 
     @Param
     Integer port = 8080;
@@ -34,13 +35,14 @@ public class WebFrontend {
     @Start
     public void startServer() {
         try {
-            mhandler = new ModelServiceSocketHandler(modelService, delayWhenNothing, maxRetry);
+            mhandler = new ModelServiceSocketHandler(modelService);
             webServer = WebServers.createWebServer(port)
-                    .add(new MetaDataHandler(modelService))
+                    .add(this)
                     .add("/model/service", mhandler)
                     .add(new EmbedHandler()) // path to web content
                     .start()
                     .get();
+            Log.info("Cloud Web Interface started on http://localhost:{}", port);
         } catch (Exception e) {
             Log.error("Error while starting Kloud Web front end", e);
         }
@@ -50,6 +52,17 @@ public class WebFrontend {
     public void stopServer() {
         webServer.stop();
         mhandler.destroy();
+    }
+
+    @Override
+    public void handleHttpRequest(HttpRequest httpRequest, HttpResponse httpResponse, HttpControl httpControl) throws Exception {
+        if (httpRequest.uri().equals("/metadata/nodeName")) {
+            httpResponse.charset(Charset.forName("UTF-8"));
+            httpResponse.content(modelService.getNodeName());
+            httpResponse.end();
+            return;
+        }
+        httpControl.nextHandler();
     }
 
 }
