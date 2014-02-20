3459313ec778ed5488bdf5d7b0581675e9d973d1
Francois Fouquet <fouquet.f@gmail.com>
2014-02-20 17:20:49 +0100

    push

--- java/org.kevoree.library.java.javaNode/src/test/java/org/kevoree/library/java/node/test/SubChildrenTest.java
+++ java/org.kevoree.library.java.javaNode/src/test/java/org/kevoree/library/java/node/test/SubChildrenTest.java
@@ -7,7 +7,6 @@ import org.kevoree.tools.test.KevoreeTestCase;
  * Created by duke on 13/02/2014.
  */
 public class SubChildrenTest extends KevoreeTestCase {
-
     @Test
     public void startupChildTest() throws Exception {
         bootstrap("node0", "oneChild.kevs");
@@ -19,5 +18,4 @@ public class SubChildrenTest extends KevoreeTestCase {
         assert (getCurrentModel("node0").findNodesByID("child1").getStarted() == true);
         waitLog("node0", "node0/child1/* INFO: Bootstrap completed", 10000);
     }
-
 }
