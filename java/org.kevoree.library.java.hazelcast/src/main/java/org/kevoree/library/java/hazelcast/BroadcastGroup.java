ecf8968fe247df89cad2b1ee50d9ad9708faa516
Erwan Daubert <erwan.daubert@gmail.com>
2014-03-13 21:35:16 +0100

    update Broadcastgroup

--- java/org.kevoree.library.java.hazelcast/src/main/java/org/kevoree/library/java/hazelcast/BroadcastGroup.java
+++ java/org.kevoree.library.java.hazelcast/src/main/java/org/kevoree/library/java/hazelcast/BroadcastGroup.java
@@ -8,10 +8,8 @@ import org.kevoree.api.Context;
 import org.kevoree.api.ModelService;
 import org.kevoree.api.handler.ModelListener;
 import org.kevoree.api.handler.UpdateCallback;
-import org.kevoree.cloner.DefaultModelCloner;
 import org.kevoree.compare.DefaultModelCompare;
 import org.kevoree.log.Log;
-import org.kevoree.modeling.api.ModelCloner;
 import org.kevoree.modeling.api.compare.ModelCompare;
 import org.kevoree.modeling.api.trace.TraceSequence;
 import org.kevoree.trace.DefaultTraceSequence;
@@ -55,7 +53,7 @@ public class BroadcastGroup implements MessageListener, ModelListener {
         Log.info("{} stopped", context.getInstanceName());
     }
 
-    private ModelCloner cloner = new DefaultModelCloner();
+//    private ModelCloner cloner = new DefaultModelCloner();
 
     @Override
     public void onMessage(Message message) {
@@ -64,8 +62,8 @@ public class BroadcastGroup implements MessageListener, ModelListener {
             try {
                 TraceSequence newtraceSeq = new DefaultTraceSequence();
                 newtraceSeq.populateFromString(message.getMessageObject().toString());
-                ContainerRoot clonedModel = cloner.clone(modelService.getCurrentModel().getModel());
-                newtraceSeq.applyOn(clonedModel);
+                /*ContainerRoot clonedModel = cloner.clone(modelService.getCurrentModel().getModel());
+                newtraceSeq.applyOn(clonedModel);*/
                 modelService.submitSequence(newtraceSeq, new UpdateCallback() {
                     @Override
                     public void run(Boolean applied) {
