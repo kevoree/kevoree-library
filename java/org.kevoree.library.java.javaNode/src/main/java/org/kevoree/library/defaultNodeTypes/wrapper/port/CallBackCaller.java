267257a1fd28b26258c8356a98c2728c6b326fd4
Erwan Daubert <erwan.daubert@gmail.com>
2014-04-28 14:52:08 +0200

    add cause exception

--- java/org.kevoree.library.java.javaNode/src/main/java/org/kevoree/library/defaultNodeTypes/wrapper/port/CallBackCaller.java
+++ java/org.kevoree.library.java.javaNode/src/main/java/org/kevoree/library/defaultNodeTypes/wrapper/port/CallBackCaller.java
@@ -2,10 +2,7 @@ package org.kevoree.library.defaultNodeTypes.wrapper.port;
 
 import org.kevoree.api.Callback;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.lang.reflect.ParameterizedType;
-import java.lang.reflect.Type;
+import java.lang.reflect.*;
 
 /**
  * Created by duke on 10/12/2013.
@@ -21,9 +18,9 @@ public class CallBackCaller {
             callback.onSuccess(result);
         } catch (Exception e) {
             if (result != null) {
-                callback.onError(new Exception("Bad Callback parameter " + result.getClass().getName()));
+                callback.onError(new Exception("Bad Callback parameter " + result.getClass().getName(), e));
             } else {
-                callback.onError(new Exception("Bad Callback parameter for null"));
+                callback.onError(new Exception("Bad Callback parameter for null", e));
             }
         }
     }
