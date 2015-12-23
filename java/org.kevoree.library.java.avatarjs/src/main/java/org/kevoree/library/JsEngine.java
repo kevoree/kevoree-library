ed6a6be41ff961c18bef1489b4b7199343cce4d5
Manuel Leduc <manuel.leduc@inria.fr>
2015-12-23 09:43:00 +0100

    Javascript object result usage example added to unit tests + Internal API cleanup + pom versions updated

--- java/org.kevoree.library.java.avatarjs/src/main/java/org/kevoree/library/JsEngine.java
+++ java/org.kevoree.library.java.avatarjs/src/main/java/org/kevoree/library/JsEngine.java
@@ -1,5 +1,7 @@
 package org.kevoree.library;
 
+import jdk.nashorn.api.scripting.ScriptObjectMirror;
+
 import javax.script.Invocable;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
@@ -9,12 +11,10 @@ import javax.script.ScriptException;
  * Created by mleduc on 18/11/15.
  */
 public class JsEngine {
-    public Object evaluateFunction(final String expression, final String functionName, final String arg) throws ScriptException, NoSuchMethodException {
+    public String evaluateFunction(final String expression, final String functionName, final String arg) throws ScriptException, NoSuchMethodException {
         final ScriptEngineManager manager = new ScriptEngineManager();
         final ScriptEngine engine = manager.getEngineByName("nashorn");
-
         engine.eval(expression);
-        final Invocable inv = (Invocable) engine;
-        return inv.invokeFunction(functionName, arg);
+        return String.valueOf(((Invocable) engine).invokeFunction(functionName, arg));
     }
 }
--- java/org.kevoree.library.java.avatarjs/src/test/java/org/kevoree/library/JsEngineTest.java
+++ java/org.kevoree.library.java.avatarjs/src/test/java/org/kevoree/library/JsEngineTest.java
@@ -11,25 +11,31 @@ public class JsEngineTest {
 
     @Test
     public void testBasic() throws Exception {
-        final Object res = jsEngine.evaluateFunction("function a() { return 1; }", "a", null);
-        Assert.assertEquals(1, res);
+        final String res = jsEngine.evaluateFunction("function a() { return 1; }", "a", null);
+        Assert.assertEquals("1", res);
     }
 
     @Test
     public void testCastStringParam1() throws Exception {
-        final Object res = jsEngine.evaluateFunction("function a(z) { return z+1; }", "a", "1");
+        final String res = jsEngine.evaluateFunction("function a(z) { return z+1; }", "a", "1");
         Assert.assertEquals("11", res);
     }
 
     @Test
     public void testCastStringParam2() throws Exception {
-        final Object res = jsEngine.evaluateFunction("function a(z) { return parseInt(z)+1; }", "a", "1.0");
-        Assert.assertEquals(2.0, res);
+        final String res = jsEngine.evaluateFunction("function a(z) { return parseInt(z)+1; }", "a", "1.0");
+        Assert.assertEquals("2.0", res);
     }
 
     @Test(expected = java.lang.NoSuchMethodException.class)
     public void testWrongFunctionName() throws Exception {
-        final Object res = jsEngine.evaluateFunction("function a() { return 1; }", "b", null);
+        final String res = jsEngine.evaluateFunction("function a() { return 1; }", "b", null);
         Assert.assertEquals(1, res);
+    }
+
+    @Test
+    public void testReturnJsonObject() throws Exception {
+        final String o = jsEngine.evaluateFunction("function a(x) { return JSON.stringify({\"foo\": x }); }", "a", "bar");
+        Assert.assertEquals("{\"foo\":\"bar\"}", o);
     }
 }
