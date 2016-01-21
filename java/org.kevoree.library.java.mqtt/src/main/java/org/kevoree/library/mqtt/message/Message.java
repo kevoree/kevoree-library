be2dc33431b28c964b9e39d5fe7c4d919b198efc
Maxime Tricoire <max.tricoire@gmail.com>
2016-01-21 16:08:10 +0100

    clean

--- java/org.kevoree.library.java.mqtt/src/main/java/org/kevoree/library/mqtt/message/Message.java
+++ java/org.kevoree.library.java.mqtt/src/main/java/org/kevoree/library/mqtt/message/Message.java
@@ -8,20 +8,4 @@ public class Message {
 
     public String topic;
     public String message;
-
-    public String getTopic() {
-        return topic;
-    }
-
-    public void setTopic(String topic) {
-        this.topic = topic;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
 }
