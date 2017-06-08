e44a38ab3c2059d214db21e2da0555d288ac0f94
maxleiko <max.tricoire@gmail.com>
2017-06-08 11:50:23 +0200

    add mvnw

--- atomic-deploy.sh
+++ atomic-deploy.sh
@@ -5,5 +5,5 @@
 # inexisting artifact jar such as "010203-5" and available jar is one second
 # away from perfection "010202-5.jar" which sucks hard
 find . -maxdepth 1 -type d -regextype sed -regex ".*/[^.].*" | while read d; do
-  (cd $d && mvn deploy)
+  (cd $d && ./mvnw deploy)
 done
